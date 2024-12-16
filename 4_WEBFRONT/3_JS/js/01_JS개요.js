54d9ce287f1dd4287aad0f4973de3120de1d5bd2
정택정 <jtjjang2025@gmail.com>
2024-12-16 17:47:36 +0900

    2024-12-16 수업

---
+++ 4_WEBFRONT/3_JS/js/01_JS개요.js
@@ -0,0 +1,17 @@
+// 한줄 주석
+/* 범위 주석 */
+
+// js파일은 <script>태그 내부라고 생각하면됨
+
+
+function btnClick2(){
+    window.alert("external 방식 알림창")
+}
+
+function changeColor1(){
+    document.getElementById("box").style.backgroundColor ="red";
+}
+
+function changeColor2(){
+    document.getElementById("box").style.backgroundColor="blue";
+}
